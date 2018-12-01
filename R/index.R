7133e577e274b46d314e6651bda54962fa0cef3f
Bill Behrman <behrman@stanford.edu>
2018-11-30 18:39:01 -0800

    Add column to syllabus

--- R/index.R
+++ R/index.R
@@ -1,6 +1,6 @@
-themes <- c("Explore", "Wrangle", "Visualize", "Model", "Program", "Communicate", "Workflow", "          ")
+themes <- c("Explore", "Wrangle", "Visualize", "Model", "Program", "Communicate", "Workflow")
 
-unit_row <- function(unit, title, updated, theme, week = "", n = 3) {
+unit_row <- function(unit, title, updated, theme, week = "", n = 4) {
   ncol <- length(themes) + n
   start_col <- match(theme, tolower(themes))
   end_col <- start_col + n
@@ -65,6 +65,7 @@ theme_index <- function(weeks, unit_index = load_units(), reverse = FALSE) {
     <table class="syllabus">
     <colgroup>
       <col class="week" />
+      <col class="theme" />
       <col class="theme" />
       <col class="theme" />
       <col class="theme" />
